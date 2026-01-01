5ea4adf64f0e4cc4bb217c52a2d0cf978863854c
utsav-kumar7856891309 <15102004utsavkumar@gmail.com>
2026-01-01 14:59:43 +0530

    re-intital frontent

---
+++ frontent/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="text-red-500">Welcome to the MERN Chat App </p>
+      <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-info">Info</button>
+        <button className="btn btn-success">Success</button>
+        <button className="btn btn-warning">Warning</button>
+        <button className="btn btn-error">Error</button>
+    </>
+  )
+}
+
+export default App
